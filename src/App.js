eff0155a51335d2ca4fcd066cd461cfb76597df5
tonnysanjidaakter <tonnysanjidaakter@gmail.com>
2022-04-20 22:09:34 +0600

    setup auth commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,67 @@
-import logo from './logo.svg';
+
 import './App.css';
+import app from './firebase.init';
+import {getAuth, GithubAuthProvider, GoogleAuthProvider, signInWithPopup, signOut} from 'firebase/auth'
+import { useState } from 'react';
+
+const auth = getAuth(app);
 
 function App() {
+  const [user, setUser] = useState({});
+  const googleProvider = new GoogleAuthProvider();
+  const githubProvider = new GithubAuthProvider(); 
+  
+  const handleGoogleSignIn = () =>{
+    signInWithPopup(auth, googleProvider)
+    .then(result =>{
+      const user = result.user;
+      setUser(user);
+      console.log(user);
+    })
+    .catch(error =>{
+      console.error('error', error); 
+    })
+    // console.log('working')
+  }
+
+  const handleGithubSignIn = () =>{
+    signInWithPopup(auth, githubProvider)
+    .then(result =>{
+      const user = result.user;
+      setUser(user);
+      console.log(user)
+    })
+    .catch(error => {
+      console.error(error);
+    })
+  }
+
+  const handleSignOut = () =>{
+    signOut(auth)
+    .then( () => {
+      setUser({});
+    })
+    .catch(error =>{
+      setUser({});
+    })
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* { condition ? true : false} */}
+      {
+        user.uid ? <button onClick={handleSignOut}>Sign out</button>
+        :
+        <>
+          <button onClick={handleGoogleSignIn}>Google Sing In</button>
+        <button onClick={handleGithubSignIn}>Github Sign In</button>
+        </>
+      
+      }
+
+      <h2>Name: {user.displayName}</h2>
+      <p>I know your email address: {user.email}</p>
+      <img src={user.photoURL} alt="" />
     </div>
   );
 }
---
+++ src/firebase.init.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCNgi6d0nRDos1oTYaNmNGHMuMdYkQnrfs",
+  authDomain: "simple-firebase-authenti-fb698.firebaseapp.com",
+  projectId: "simple-firebase-authenti-fb698",
+  storageBucket: "simple-firebase-authenti-fb698.appspot.com",
+  messagingSenderId: "176372166066",
+  appId: "1:176372166066:web:d85d8488bccb92e09fc8ab"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
